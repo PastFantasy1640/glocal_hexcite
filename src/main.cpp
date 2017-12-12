9f0d0ce48518c36a2ed75fccb8f6b1d80566c2ea
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-13 03:18:13 +0900

    メイン関数からnetworkの設定シーケンスを呼び出し

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 
 #include "game/SprashScreens.hpp"
+#include "network/Network.hpp"
 
 int main(void) {
 	gh::drawSprashScreen();
 
+	bool is_online = true;
+
+	std::cout << std::endl;
+	std::cout << "オンラインで遊びますか？オフラインで遊びますか？" << std::endl;
+	std::cout << "オンライン:1 / オフライン:0 ? 1" << std::endl;
+	std::cout << "（現在オフラインは未実装なため、強制的にオンラインを選択しています）" << std::endl;
+
+	if (is_online) {
+		std::unique_ptr<gh::Network> network = gh::Network::setupOnline();
+		if (network) {
+			//成功
+
+		}
+	}
 
 
 	return 0;
