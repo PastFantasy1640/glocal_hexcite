129a7cffb62479f31460e79bb828e1ce3cdef9d7
wgprojects2013 <pastfantasy1640@gmail.com>
2017-12-15 00:34:08 +0900

    デバッグ用にコメントアウト

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,15 @@ int main(void) {
 	std::cout << "オンラインで遊びますか？オフラインで遊びますか？" << std::endl;
 	std::cout << "オンライン:1 / オフライン:2 / 終了:0 ? 1" << std::endl;
 	std::cout << "（現在オフラインは未実装なため、強制的にオンラインを選択しています）" << std::endl;
-
+	/*
 	if (is_online) {
 		std::unique_ptr<gh::Network> network = gh::Network::setupOnline();
 		if (network) {
 			//成功
 			gh::MatchingScene(std::move(network));
 		}
-	}
+	}*/
 
-
+	gh::MatchingScene(nullptr);
 	return 0;
 }
